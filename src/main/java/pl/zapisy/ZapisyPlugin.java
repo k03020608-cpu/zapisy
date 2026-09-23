ab312b9f13def8456623ad64b1876e8bd824e7e4
k03020608-cpu <k03020608@gmail.com>
2026-09-23 14:13:47 +0200

    Update ZapisyPlugin.java

--- src/main/java/pl/zapisy/ZapisyPlugin.java
+++ src/main/java/pl/zapisy/ZapisyPlugin.java
@@ -6,14 +6,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class ZapisyPlugin extends JavaPlugin {
 
     private ZapisyManager manager;
+    private TurniejManager turniej;
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
         manager = new ZapisyManager(this);
         manager.load();
 
-        ZapisyCommands commands = new ZapisyCommands(manager);
-        for (String name : new String[]{"zapisz", "wypisz", "zapisani"}) {
+        turniej = new TurniejManager(this);
+        turniej.load();
+
+        ZapisyCommands commands = new ZapisyCommands(manager, turniej);
+        String[] nazwyKomend = {
+                "zapisz", "wypisz", "zapisani",
+                "resetlisty", "zamknijzapisy", "otworzzapisy",
+                "stworzturniej", "wygral", "turniej"
+        };
+        for (String name : nazwyKomend) {
             PluginCommand command = getCommand(name);
             if (command != null) {
                 command.setExecutor(commands);
